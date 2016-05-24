3aca71cafe8c6ea67aa4e1ce465e73362c354fd0
jflute <dbflute@gmail.com>
2016-05-24 12:59:57 +0900

    review deco's first action

--- src/main/java/org/dbflute/intro/app/web/client/ClientDfpropAction.java
+++ src/main/java/org/dbflute/intro/app/web/client/ClientDfpropAction.java
@@ -1,12 +1,5 @@
 package org.dbflute.intro.app.web.client;
 
-import org.dbflute.intro.app.logic.simple.DbFluteIntroLogic;
-import org.dbflute.intro.app.web.base.IntroBaseAction;
-import org.dbflute.intro.mylasta.exception.FileNotFoundException;
-import org.dbflute.intro.mylasta.exception.FileNotReadException;
-import org.lastaflute.web.Execute;
-import org.lastaflute.web.response.JsonResponse;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -14,6 +7,13 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.dbflute.intro.app.logic.simple.DbFluteIntroLogic;
+import org.dbflute.intro.app.web.base.IntroBaseAction;
+import org.dbflute.intro.mylasta.exception.FileNotFoundException;
+import org.dbflute.intro.mylasta.exception.FileNotReadException;
+import org.lastaflute.web.Execute;
+import org.lastaflute.web.response.JsonResponse;
 
 /**
  * @author deco
@@ -28,44 +28,44 @@ public class ClientDfpropAction extends IntroBaseAction {
     // ===================================================================================
     //                                                                             Execute
     //                                                                             =======
-    // TODO DONE deco pri.A migration to ClientDfpropAction by jflute (2016/05/09)
-    // TODO DONE deco pri.B don't use throws (IOException) by jflute (2016/05/09)
     @Execute
     public JsonResponse<List<ClientDfpropBean>> index(String project) {
         // find dfprop files.
+        // TODO deco to private method by jflute (2016/05/24)
         File dfpropDir = new File(DbFluteIntroLogic.BASE_DIR_PATH, "dbflute_" + project + "/dfprop");
-        // TODO DONE deco pri.B file filtering by jflute (2016/05/09)
-        // e.g. File[] dfpropFiles = dfpropDir.listFiles((dir, name) -> name.endsWith(".dfprop"));
-        // TODO DONE deco pri.B rename files to dfpropFiles by jflute (2016/05/09)
         File[] dfpropFiles = dfpropDir.listFiles((dir, name) -> name.endsWith(".dfprop"));
         if (dfpropFiles == null || dfpropFiles.length == 0) {
-            // TODO deco pri.C BusinessException by jflute (2016/05/09)
             throw new FileNotFoundException("Not found dfprop files. file dir=" + dfpropDir.getPath());
         }
 
         // create display bean.
-        List<ClientDfpropBean> bean = mappingToBean(dfpropFiles);
+        List<ClientDfpropBean> beans = mappingToBeans(dfpropFiles);
 
-        return asJson(bean);
+        return asJson(beans);
     }
 
-    private List<ClientDfpropBean> mappingToBean(File[] dfpropFiles) {
-        // TODO DONE deco pri.C use List interface by jflute (2016/05/09)
-        // TODO deco pri.C use stream by jflute (2016/05/09)
+    private List<ClientDfpropBean> mappingToBeans(File[] dfpropFiles) {
         // ↑ For returning an exception, I don't use stream.
+        // TODO deco like this by jflute (2016/05/24)
+        //return Stream.of(dfpropFiles).map(dfpropFile -> {
+        //    String fileText;
+        //    try {
+        //        fileText = FileUtils.readFileToString(dfpropFile, UTF8);
+        //    } catch (IOException e) {
+        //        throw new LaSystemException("Cannot read the file: " + dfpropFile);
+        //    }
+        //    return new ClientDfpropBean(dfpropFile.getName(), fileText);
+        //}).collect(Collectors.toList());
+
         List<ClientDfpropBean> dfpropList = new ArrayList<>();
         for (File dfpropFile : dfpropFiles) {
             ClientDfpropBean bean = new ClientDfpropBean();
             bean.fileName = dfpropFile.getName();
-            // TODO deco pri.C don't depend on view, with refactoring logic by jflute (2016/05/09)
-            // TODO DONE deco pri.B avoid mojibake by jflute (2016/05/09)
             try {
                 BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(dfpropFile), UTF8));
                 bean.content = br.lines()
-                    .collect(StringBuilder::new
-                        , (stringBuilder, str) -> stringBuilder.append(str).append(System.lineSeparator())
-                        , StringBuilder::append)
-                    .toString();
+                        .collect(StringBuilder::new, (stringBuilder, str) -> stringBuilder.append(str).append("\n"), StringBuilder::append)
+                        .toString();
                 dfpropList.add(bean);
             } catch (UnsupportedEncodingException | java.io.FileNotFoundException e) {
                 throw new FileNotReadException(e.getMessage(), e);
