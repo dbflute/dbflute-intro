27f1c0dd235a7e297d775b5edf9b742f2a25bb60
p1us2er0 <p1us3inus2er0@gmail.com>
2016-11-06 22:59:15 +0900

    add copyright.

--- src/main/java/org/dbflute/intro/app/web/welcome/WelcomeAction.java
+++ src/main/java/org/dbflute/intro/app/web/welcome/WelcomeAction.java
@@ -1,4 +1,23 @@
+/*
+ * Copyright 2014-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.dbflute.intro.app.web.welcome;
+
+import java.util.LinkedHashMap;
+
+import javax.annotation.Resource;
 
 import org.dbflute.intro.app.logic.client.ClientInfoLogic;
 import org.dbflute.intro.app.logic.client.ClientUpdateLogic;
@@ -15,9 +34,6 @@ import org.dbflute.intro.app.web.base.IntroBaseAction;
 import org.dbflute.optional.OptionalThing;
 import org.lastaflute.web.Execute;
 import org.lastaflute.web.response.JsonResponse;
-
-import javax.annotation.Resource;
-import java.util.LinkedHashMap;
 
 /**
  * @author hakiba
