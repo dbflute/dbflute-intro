0c8f028e757799358f8f7f81593aeb704a9c2714
Akifumi.Tominaga <tominaga.switch@gmail.com>
2016-12-30 01:35:05 +0900

    handling not synchronized exception

---
+++ src/main/java/org/dbflute/intro/app/web/task/TaskExecuteResult.java
@@ -0,0 +1,12 @@
+package org.dbflute.intro.app.web.task;
+
+/**
+ * @author deco
+ */
+public class TaskExecuteResult {
+    public final boolean result;
+
+    public TaskExecuteResult(boolean result) {
+        this.result = result;
+    }
+}
