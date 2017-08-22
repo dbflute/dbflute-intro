684e30ceee55d8d470b4921aa0518c1fc2d49885
hakiba <h031203yama@gmail.com>
2017-08-22 09:06:05 +0900

    add files for decoment test and override setup in testcase

--- src/test/java/org/dbflute/intro/app/logic/document/decomment/DocumentDecommentPhysicalLogicTest.java
+++ src/test/java/org/dbflute/intro/app/logic/document/decomment/DocumentDecommentPhysicalLogicTest.java
@@ -1,5 +1,8 @@
 package org.dbflute.intro.app.logic.document.decomment;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.dbflute.intro.app.model.document.decomment.DfDecoMapPickup;
 import org.dbflute.intro.unit.UnitIntroTestCase;
 import org.junit.Test;
@@ -9,17 +12,44 @@ import org.junit.Test;
  */
 public class DocumentDecommentPhysicalLogicTest extends UnitIntroTestCase {
 
+    // ===================================================================================
+    //                                                                          Definition
+    //                                                                          ==========
+    @SuppressWarnings("FieldCanBeLocal")
+    private static String TEST_RESOURCE_PICKUP_FILE_PATH = "/src/test/resources/schema/decomment/pickup/decomment-pickup.dfmap";
+    @SuppressWarnings("FieldCanBeLocal")
+    private static String TEST_RESOURCE_PICKUP_PATH = "/src/test/resources/schema/decomment/piece";
+    @SuppressWarnings("FieldCanBeLocal")
+    private static String PICKUP_FILE_PATH = "/schema/decomment/pickup/decomment-pickup.dfmap";
+    @SuppressWarnings("FieldCanBeLocal")
+    private static String PIECE_DIR_PATH = "/schema/decomment/piece";
+
+    // ===================================================================================
+    //                                                                            Override
+    //                                                                            ========
+    // TODO hakiba fix duplicate by hakiba (2017/08/22)
+    @SuppressWarnings("Duplicates")
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        File srcPickupFile = new File(getProjectDir(), TEST_RESOURCE_PICKUP_FILE_PATH);
+        File srcPieceDir = new File(getProjectDir(), TEST_RESOURCE_PICKUP_PATH);
+        File destPickupFile = new File(getProjectDir(), TEST_CLIENT_PATH + PICKUP_FILE_PATH);
+        File destPieceDir = new File(getProjectDir(), TEST_CLIENT_PATH + PIECE_DIR_PATH);
+        FileUtils.copyFile(srcPickupFile, destPickupFile);
+        FileUtils.copyDirectory(srcPieceDir, destPieceDir);
+    }
+
     @Test
     public void test_readMergedDecommentPickupMap() {
-        // TODO hakiba put test data in test/resources by hakiba (2017/08/18)
+        // TODO done hakiba put test data in test/resources by hakiba (2017/08/18)
         // ## Arrange ##
         DocumentDecommentPhysicalLogic logic = new DocumentDecommentPhysicalLogic();
         inject(logic);
 
         // ## Act ##
-        // TODO hakiba use testdb by jflute (2017/08/17)
-        String clientProject = "introdb";
-        DfDecoMapPickup pickup = logic.readMergedDecommentPickupMap(clientProject);
+        // TODO done hakiba use testdb by jflute (2017/08/17)
+        DfDecoMapPickup pickup = logic.readMergedDecommentPickupMap(TEST_CLIENT_PROJECT);
 
         // ## Assert ##
         // Assert by visual confirmation
--- src/test/java/org/dbflute/intro/app/web/document/decomment/DocumentDecommentActionTest.java
+++ src/test/java/org/dbflute/intro/app/web/document/decomment/DocumentDecommentActionTest.java
@@ -1,8 +1,10 @@
 package org.dbflute.intro.app.web.document.decomment;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
 
+import org.apache.commons.io.FileUtils;
 import org.dbflute.intro.app.web.document.decomment.DecommentPostBody.DecommentTablePart;
 import org.dbflute.intro.app.web.document.decomment.DecommentPostBody.DecommentTablePart.DecommentColumnPart;
 import org.dbflute.intro.unit.UnitIntroTestCase;
@@ -14,6 +16,35 @@ import org.lastaflute.web.response.JsonResponse;
  */
 public class DocumentDecommentActionTest extends UnitIntroTestCase {
 
+    // ===================================================================================
+    //                                                                          Definition
+    //                                                                          ==========
+    @SuppressWarnings("FieldCanBeLocal")
+    private static String TEST_RESOURCE_PICKUP_FILE_PATH = "/src/test/resources/schema/decomment/pickup/decomment-pickup.dfmap";
+    @SuppressWarnings("FieldCanBeLocal")
+    private static String TEST_RESOURCE_PICKUP_PATH = "/src/test/resources/schema/decomment/piece";
+    @SuppressWarnings("FieldCanBeLocal")
+    private static String PICKUP_FILE_PATH = "/schema/decomment/pickup/decomment-pickup.dfmap";
+    @SuppressWarnings("FieldCanBeLocal")
+    private static String PIECE_DIR_PATH = "/schema/decomment/piece";
+
+    // ===================================================================================
+    //                                                                            Override
+    //                                                                            ========
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        File srcPickupFile = new File(getProjectDir(), TEST_RESOURCE_PICKUP_FILE_PATH);
+        File srcPieceDir = new File(getProjectDir(), TEST_RESOURCE_PICKUP_PATH);
+        File destPickupFile = new File(getProjectDir(), TEST_CLIENT_PATH + PICKUP_FILE_PATH);
+        File destPieceDir = new File(getProjectDir(), TEST_CLIENT_PATH + PIECE_DIR_PATH);
+        FileUtils.copyFile(srcPickupFile, destPickupFile);
+        FileUtils.copyDirectory(srcPieceDir, destPieceDir);
+    }
+
+    // ===================================================================================
+    //                                                                                Test
+    //                                                                                ====
     @Test
     public void test_save_createHakiMap() throws Exception {
         // ## Arrange ##
@@ -56,12 +87,12 @@ public class DocumentDecommentActionTest extends UnitIntroTestCase {
     @Test
     public void test_read_PickupResult() throws Exception {
         // ## Arrange ##
-        // TODO hakiba put test data in test/resources by hakiba (2017/08/18)
+        // TODO done hakiba put test data in test/resources by hakiba (2017/08/18)
         DocumentDecommentAction action = new DocumentDecommentAction();
         inject(action);
 
         // ## Act ##
-        JsonResponse<DecommentPickupResult> response = action.pickup("introdb");
+        JsonResponse<DecommentPickupResult> response = action.pickup(TEST_CLIENT_PROJECT);
 
         // ## Assert ##
         // TODO done hakiba like this by jflute (2017/08/17)
