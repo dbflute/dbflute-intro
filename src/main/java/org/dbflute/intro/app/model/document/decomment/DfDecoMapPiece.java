792d6886372b025fd1064e4022f2e539b03487e8
EgumaYuto <yuto.eguma@gmail.com>
2017-11-11 14:53:34 +0900

    fix deco map piece structure

--- src/main/java/org/dbflute/intro/app/model/document/decomment/DfDecoMapPiece.java
+++ src/main/java/org/dbflute/intro/app/model/document/decomment/DfDecoMapPiece.java
@@ -1,12 +1,13 @@
 package org.dbflute.intro.app.model.document.decomment;
 
+import static org.dbflute.intro.mylasta.appcls.AppCDef.PieceTargetType;
+
+import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.dbflute.helper.mapstring.MapListString;
-import org.dbflute.intro.app.model.document.decomment.parts.DfDecoMapTablePart;
 
 /**
  * @author hakiba
@@ -17,84 +18,46 @@ public class DfDecoMapPiece {
     // ===================================================================================
     //                                                                          Definition
     //                                                                          ==========
-    private static final String DECO_MAP_KEY = "tableList";
+    private static String DEFAULT_FORMAT_VERSION = "1.0";
 
     // ===================================================================================
-    //                                                                           Attribute
     //                                                                           =========
-    protected String author;
+    //                                                                           Attribute
     protected boolean merged;
-    protected String formatVersion;
-    protected Map<String, List<DfDecoMapTablePart>> decoMap;
+    protected String tableName;
+    protected String columnName;
+    protected PieceTargetType targetType;
+    protected String decomment;
+    protected String databaseComment;
+    protected Long commentVersion;
+    protected List<String> authorList;
+    protected String pieceCode;
+    protected LocalDateTime pieceDatetime;
+    protected String pieceOwner;
+    protected List<String> previousPieceList;
+    protected String formatVersion = DEFAULT_FORMAT_VERSION;
 
     // done cabos move to before accessor by jflute (2017/08/10)
     // ===================================================================================
     //                                                                           Converter
     //                                                                           =========
-    //    [e.g. piece map, table decomment edited]
-    //    map:{
-    //        ; author = hakiba
-    //        ; merged = false
-    //        ; formatVersion = 1.0
-    //        ; decoMap = map:{
-    //            ; tableList = list:{
-    //                ; map:{
-    //                    ; tableName = MEMBER
-    //                    ; propertyList = list:{
-    //                        ; map:{
-    //                            ; decomment = piece column decomment
-    //                            ; databaseComment = sea mystic land oneman
-    //                            ; commentVersion = 2 // incremented
-    //                            ; authorList = list:{ cabos, hakiba, deco, jflute }
-    //                            ; pieceDatetime = 2017-08-16T23:26:36.690
-    //                        }
-    //                    }
-    //                    ; columnList = list:{}  // empty
-    //                }
-    //            }
-    //        }
-    //    }
-    //
-    //    [e.g. piece map, column decomment edited]
-    //    map:{
-    //        ; author = hakiba
-    //        ; merged = false
-    //        ; formatVersion = 1.0
-    //        ; decoMap = map:{
-    //            ; tableList = list:{
-    //                ; map:{
-    //                    ; tableName = MEMBER
-    //                    ; propertyList = list:{}  // empty
-    //                    ; columnList = list:{
-    //                        ; map:{
-    //                            ; columnName = MEMBER_NAME
-    //                            ; propertyList = list:{
-    //                                ; map:{
-    //                                    ; decomment = sea mystic land oneman
-    //                                    ; databaseComment = sea mystic
-    //                                    ; commentVersion = 2 // incremented
-    //                                    ; authorList = list:{ cabos, hakiba, deco, jflute }
-    //                                    ; pieceDatetime = 2017-08-16T23:26:36.690
-    //                                }
-    //                            }
-    //                        }
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    }
+    // TODO yuto write e.g. (2017/11/11)
     public Map<String, Object> convertMap() {
         // done cabos Map by jflute (2017/08/10)
         // done cabos use Linked by jflute (2017/09/07)
         Map<String, Object> map = new LinkedHashMap<>();
-        map.put("author", this.author);
         map.put("merged", this.merged);
+        map.put("tableName", this.tableName);
+        map.put("columnName", this.columnName);
+        map.put("decomment", this.decomment);
+        map.put("databaseComment", this.databaseComment);
+        map.put("commentVersion", this.commentVersion);
+        map.put("authorList", this.authorList);
+        map.put("pieceCode", this.pieceCode);
+        map.put("pieceDatetime", this.pieceDatetime);
+        map.put("pieceOwner", this.pieceOwner);
+        map.put("previousPieceList", this.previousPieceList);
         map.put("formatVersion", this.formatVersion);
-        List<Map<String, Object>> tableList =
-            this.decoMap.get(DECO_MAP_KEY).stream().map(table -> table.convertPieceMap()).collect(Collectors.toList());
-        LinkedHashMap<Object, Object> decoMap = new LinkedHashMap<>();
-        decoMap.put(DECO_MAP_KEY, tableList);
-        map.put("decoMap", decoMap);
         return map;
     }
 
@@ -109,45 +72,103 @@ public class DfDecoMapPiece {
     // ===================================================================================
     //                                                                            Accessor
     //                                                                            ========
-    public String getFormatVersion() {
-        return formatVersion;
+    public void setMerged(boolean merged) {
+        this.merged = merged;
     }
 
-    public void setFormatVersion(String formatVersion) {
-        this.formatVersion = formatVersion;
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
     }
 
-    public String getAuthor() {
-        return author;
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
+    public void setTargetType(PieceTargetType targetType) {
+        this.targetType = targetType;
+    }
+
+    public void setDecomment(String decomment) {
+        this.decomment = decomment;
+    }
+
+    public void setDatabaseComment(String databaseComment) {
+        this.databaseComment = databaseComment;
+    }
+
+    public void setCommentVersion(Long commentVersion) {
+        this.commentVersion = commentVersion;
+    }
+
+    public void setAuthorList(List<String> authorList) {
+        this.authorList = authorList;
+    }
+
+    public void setPieceCode(String pieceCode) {
+        this.pieceCode = pieceCode;
+    }
+
+    public void setPieceDatetime(LocalDateTime pieceDatetime) {
+        this.pieceDatetime = pieceDatetime;
+    }
+
+    public void setPieceOwner(String pieceOwner) {
+        this.pieceOwner = pieceOwner;
+    }
+
+    public void setPreviousPieceList(List<String> previousPieceList) {
+        this.previousPieceList = previousPieceList;
     }
 
     public boolean isMerged() {
         return merged;
     }
 
-    public void setMerged(boolean merged) {
-        this.merged = merged;
+    public String getTableName() {
+        return tableName;
     }
 
-    public Map<String, List<DfDecoMapTablePart>> getDecoMap() {
-        return decoMap;
+    public String getColumnName() {
+        return columnName;
     }
 
-    public void setDecoMap(Map<String, List<DfDecoMapTablePart>> decoMap) {
-        this.decoMap = decoMap;
+    public PieceTargetType getTargetType() {
+        return targetType;
     }
 
-    public List<DfDecoMapTablePart> getTableList() {
-        return decoMap.get(DECO_MAP_KEY);
+    public String getDecomment() {
+        return decomment;
     }
 
-    public void setTableList(List<DfDecoMapTablePart> decoMap) {
-        Map<String, List<DfDecoMapTablePart>> map = new LinkedHashMap<>();
-        map.put(DECO_MAP_KEY, decoMap);
-        this.decoMap = map;
+    public String getDatabaseComment() {
+        return databaseComment;
+    }
+
+    public Long getCommentVersion() {
+        return commentVersion;
+    }
+
+    public List<String> getAuthorList() {
+        return authorList;
+    }
+
+    public String getPieceCode() {
+        return pieceCode;
+    }
+
+    public LocalDateTime getPieceDatetime() {
+        return pieceDatetime;
+    }
+
+    public String getPieceOwner() {
+        return pieceOwner;
+    }
+
+    public List<String> getPreviousPieceList() {
+        return previousPieceList;
+    }
+
+    public String getFormatVersion() {
+        return formatVersion;
     }
 }
