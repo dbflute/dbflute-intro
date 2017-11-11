1aea0529fd3fab97fa56b667f5e0d1c1fb3c71eb
jflute <dbflute@gmail.com>
2017-11-12 08:59:43 +0900

    remove readme.txt for test decomment files

--- src/test/java/org/dbflute/intro/unit/UnitIntroTestCase.java
+++ src/test/java/org/dbflute/intro/unit/UnitIntroTestCase.java
@@ -114,6 +114,12 @@ public abstract class UnitIntroTestCase extends WebContainerTestCase {
         File destPieceDir = getTestDecommentPieceDir();
         FileUtils.copyFile(srcPickupFile, destPickupFile);
         FileUtils.copyDirectory(srcPieceDir, destPieceDir);
+        File[] unneededFiles = destPieceDir.listFiles(file -> file.isFile() && !file.getName().endsWith(".dfmap"));
+        if (unneededFiles != null) {
+            for (File unneededFile : unneededFiles) {
+                unneededFile.delete(); // e.g. readme.txt
+            }
+        }
     }
 
     protected File getTestDecommentPickupFile() {
