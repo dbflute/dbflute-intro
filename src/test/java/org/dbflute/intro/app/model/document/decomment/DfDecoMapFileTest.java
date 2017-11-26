627d812ce650e798944c7f2fe61155dcb60f6b96
tominaga <tominaga.switch@gmail.com>
2017-11-26 11:05:19 +0900

    add license header

--- src/test/java/org/dbflute/intro/app/model/document/decomment/DfDecoMapFileTest.java
+++ src/test/java/org/dbflute/intro/app/model/document/decomment/DfDecoMapFileTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.dbflute.intro.app.model.document.decomment;
 
 import java.time.LocalDateTime;
@@ -43,13 +58,16 @@ public class DfDecoMapFileTest extends UnitIntroTestCase {
         assertNotNull(result);
         log(result);
         // assert all table and column
-        long columnCount =
-            result.getTableList().stream().map(tablePart -> tablePart.getColumnList()).flatMap(columnParts -> columnParts.stream()).count();
+        long columnCount = result.getTableList()
+                .stream()
+                .map(tablePart -> tablePart.getColumnList())
+                .flatMap(columnParts -> columnParts.stream())
+                .count();
         assertEquals(3, columnCount);
     }
 
     private DfDecoMapPiece preparePiece(String tableName, String columnName, String author, long commentVersion,
-        LocalDateTime decommentDateTime) {
+            LocalDateTime decommentDateTime) {
         DfDecoMapPiece piece = new DfDecoMapPiece();
         piece.setFormatVersion("1.0");
         piece.setTableName(tableName);
