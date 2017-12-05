cf25b0224f72884f3ca6231f27193af89397eb2d
tominaga <tominaga.switch@gmail.com>
2017-12-05 22:39:26 +0900

    prepare DBFlute embedded environment for decomment

--- dbflute_introdb/manage.sh
+++ dbflute_introdb/manage.sh
@@ -6,6 +6,9 @@ cd `dirname $0`
 FIRST_ARG=$1
 SECOND_ARG=$2
 
+echo "...copying your templates"
+cp -R ./templates/doc/html/* $DBFLUTE_HOME/templates/doc/html
+
 sh $DBFLUTE_HOME/etc/cmd/_df-manage.sh $MY_PROPERTIES_PATH $FIRST_ARG $SECOND_ARG
 taskReturnCode=$?
 
