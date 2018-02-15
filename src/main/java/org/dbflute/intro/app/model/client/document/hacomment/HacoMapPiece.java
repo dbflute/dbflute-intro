4e2cd2c1e6781e5a7cbdb3f2ede8842ae0a54d55
hakiba <h031203yama@gmail.com>
2018-02-15 09:22:57 +0900

    Implements Hacomment save logic & add license header

---
+++ src/main/java/org/dbflute/intro/app/model/client/document/hacomment/HacoMapPiece.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2014-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.dbflute.intro.app.model.client.document.hacomment;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author hakiba
+ */
+public class HacoMapPiece {
+
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
+    protected final String diffDate;
+    protected final String hacomment;
+    protected final List<String> authorList;
+    protected final String pieceCode;
+    protected final String pieceOwner;
+    protected final LocalDateTime pieceDatetime;
+    protected final List<String> previousPieceList;
+
+    // ===================================================================================
+    //                                                                         Constructor
+    //                                                                         ===========
+    public HacoMapPiece(String diffDate, String hacomment, List<String> authorList, String pieceCode, String pieceOwner,
+        LocalDateTime pieceDatetime, List<String> previousPieceList) {
+        this.diffDate = diffDate;
+        this.hacomment = hacomment;
+        this.authorList = authorList;
+        this.pieceCode = pieceCode;
+        this.pieceOwner = pieceOwner;
+        this.pieceDatetime = pieceDatetime;
+        this.previousPieceList = previousPieceList;
+    }
+
+    // ===================================================================================
+    //                                                                           Converter
+    //                                                                           =========
+    public Map<String, Object> convertToMap() {
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("diffDate", this.diffDate);
+        map.put("hacomment", this.hacomment);
+        map.put("authorList", this.authorList);
+        map.put("pieceCode", this.pieceCode);
+        map.put("pieceOwner", this.pieceOwner);
+        map.put("pieceDatetime", this.pieceDatetime);
+        map.put("previousPieceList", this.previousPieceList);
+        return map;
+    }
+
+    // ===================================================================================
+    //                                                                            Accessor
+    //                                                                            ========
+
+    public String getDiffDate() {
+        return diffDate;
+    }
+    public String getHacomment() {
+        return hacomment;
+    }
+    public List<String> getAuthorList() {
+        return authorList;
+    }
+    public String getPieceCode() {
+        return pieceCode;
+    }
+    public String getPieceOwner() {
+        return pieceOwner;
+    }
+    public LocalDateTime getPieceDatetime() {
+        return pieceDatetime;
+    }
+    public List<String> getPreviousPieceList() {
+        return previousPieceList;
+    }
+}
---
+++ src/main/java/org/dbflute/intro/app/web/document/hacomment/DocumentHacommentAction.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2014-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.dbflute.intro.app.web.document.hacomment;
+
+import java.time.LocalDateTime;
+
+import javax.annotation.Resource;
+
+import org.dbflute.intro.app.logic.document.DocumentAuthorLogic;
+import org.dbflute.intro.app.logic.document.hacomment.DocumentHacommentPhysicalLogic;
+import org.dbflute.intro.app.model.client.document.hacomment.HacoMapPickup;
+import org.dbflute.intro.app.model.client.document.hacomment.HacoMapPiece;
+import org.dbflute.intro.app.web.base.IntroBaseAction;
+import org.lastaflute.core.time.TimeManager;
+import org.lastaflute.web.Execute;
+import org.lastaflute.web.response.JsonResponse;
+
+/**
+ * @author hakiba
+ */
+public class DocumentHacommentAction extends IntroBaseAction {
+
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
+    @Resource
+    private TimeManager timeManager;
+    @Resource
+    private DocumentHacommentPhysicalLogic hacommentPhysicalLogic;
+    @Resource
+    private DocumentAuthorLogic authorLogic;
+
+    // ===================================================================================
+    //                                                                             Execute
+    //                                                                             =======
+    @Execute(urlPattern = "{}/@word")
+    public JsonResponse<Void> save(String projectName, HacommentSaveBody body) {
+        validate(body, messages -> moreValidate(body));
+        // TODO hakiba add client error message by hakiba (2018/02/15)
+        // verifyOrClientError(buildDebugMessageColumnNameIsNull(body), existsColumnNameIfTargetTypeColumn(body));
+        hacommentPhysicalLogic.savePiece(projectName, mappingToHacoMapPiece(body));
+        return JsonResponse.asEmptyBody();
+    }
+
+    private void moreValidate(HacommentSaveBody body) {
+        // TODO hakiba add validate logic by hakiba (2018/02/15)
+    }
+
+    private HacoMapPiece mappingToHacoMapPiece(HacommentSaveBody body) {
+        String author = authorLogic.getAuthor();
+        LocalDateTime mappingDateTime = timeManager.currentDateTime();
+        String mappingCode = buildMappingCode(body, mappingDateTime, author);
+        return new HacoMapPiece(body.diffDate, body.haccoment, body.authors, mappingCode, author, mappingDateTime, body.previousPieces);
+    }
+
+    private String buildMappingCode(HacommentSaveBody body, LocalDateTime mappingDateTime, String author) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(body.diffDate);
+        sb.append(":").append(mappingDateTime);
+        sb.append(":").append(author);
+        return Integer.toHexString(sb.toString().hashCode());
+    }
+
+    @Execute(urlPattern = "{}/@word")
+    public JsonResponse<HaccomentPickupResult> pickup(String projectName) {
+        HacoMapPickup hacoMapPickup = hacommentPhysicalLogic.readMergedPickup(projectName);
+        return asJson(new HaccomentPickupResult(hacoMapPickup));
+    }
+}
