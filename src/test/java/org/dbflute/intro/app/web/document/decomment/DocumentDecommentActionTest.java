949f90827df7a0538f569261f9ba76a362a91ce8
hakiba <h031203yama@gmail.com>
2018-05-11 12:08:27 +0900

    fix test

--- src/test/java/org/dbflute/intro/app/web/document/decomment/DocumentDecommentActionTest.java
+++ src/test/java/org/dbflute/intro/app/web/document/decomment/DocumentDecommentActionTest.java
@@ -27,6 +27,7 @@ import java.util.stream.Collectors;
 import javax.annotation.Resource;
 
 import org.dbflute.helper.mapstring.MapListFile;
+import org.dbflute.infra.doc.decomment.DfDecoMapPiece;
 import org.dbflute.infra.doc.decomment.DfDecoMapPieceTargetType;
 import org.dbflute.intro.app.logic.document.DocumentAuthorLogic;
 import org.dbflute.intro.app.web.document.decomment.DecommentPickupResult.TablePart;
@@ -246,7 +247,7 @@ public class DocumentDecommentActionTest extends UnitIntroTestCase {
 
     private ColumnPart extractPickupColumnAsOne(TablePart member, String columnName) {
         List<ColumnPart> columnList =
-            member.columns.stream().filter(column -> column.columnName.equals(columnName)).collect(Collectors.toList());
+                member.columns.stream().filter(column -> column.columnName.equals(columnName)).collect(Collectors.toList());
         assertHasOnlyOneElement(columnList);
         return columnList.get(0);
     }
@@ -279,7 +280,7 @@ public class DocumentDecommentActionTest extends UnitIntroTestCase {
             log("  {} = {}", key, value);
         });
         // done cabos fail by formatVersion by jflute (2017/11/11)
-        assertEquals("1.0", actualMap.get("formatVersion"));
+        assertEquals(DfDecoMapPiece.DEFAULT_FORMAT_VERSION, actualMap.get("formatVersion"));
         body.mappings.forEach(mapping -> {
             assertEquals(mapping.oldTableName, actualMap.get("oldTableName"));
             assertEquals(actualMap.get("oldColumnName"), null);
@@ -319,7 +320,7 @@ public class DocumentDecommentActionTest extends UnitIntroTestCase {
             log("  {} = {}", key, value);
         });
         // done cabos fail by formatVersion by jflute (2017/11/11)
-        assertEquals("1.0", actualMap.get("formatVersion"));
+        assertEquals(DfDecoMapPiece.DEFAULT_FORMAT_VERSION, actualMap.get("formatVersion"));
         body.mappings.forEach(mapping -> {
             assertEquals(mapping.oldTableName, actualMap.get("oldTableName"));
             assertEquals(mapping.oldColumnName, actualMap.get("oldColumnName"));
