4e31fa2eae48eb8c1289616482d4895c8dedf910
jflute <dbflute@gmail.com>
2018-05-13 15:00:56 +0900

    change decomment-manage.sh to reflect-templates.sh

--- dbflute_introdb/reflect-templates.sh
+++ dbflute_introdb/reflect-templates.sh
@@ -8,10 +8,3 @@ SECOND_ARG=$2
 
 echo "...copying your templates"
 cp -R ./templates/doc/html/* $DBFLUTE_HOME/templates/doc/html
-
-sh $DBFLUTE_HOME/etc/cmd/_df-manage.sh $MY_PROPERTIES_PATH $FIRST_ARG $SECOND_ARG
-taskReturnCode=$?
-
-if [ $taskReturnCode -ne 0 ];then
-  exit $taskReturnCode;
-fi
