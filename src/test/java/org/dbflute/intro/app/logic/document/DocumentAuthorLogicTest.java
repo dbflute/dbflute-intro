369340fb76d6f7e03b40db8ee93150284ed1a750
江熊夕人 <yuto.eguma@gmail.com>
2018-08-19 13:10:49 +0900

    add test class at sumida jazz festival

---
+++ src/test/java/org/dbflute/intro/app/logic/document/DocumentAuthorLogicTest.java
@@ -0,0 +1,17 @@
+package org.dbflute.intro.app.logic.document;
+
+import org.dbflute.intro.unit.UnitIntroTestCase;
+
+/**
+ * @author cabos (at sumida jazz festival)
+ */
+public class DocumentAuthorLogicTest extends UnitIntroTestCase {
+
+    public void test_getAuthor() throws Exception {
+        // ## Arrange ##
+
+        // ## Act ##
+
+        // ## Assert ##
+    }
+}
