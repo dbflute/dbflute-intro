48a5ea49d9c33262d6a3a5f74836db97c7d25713
江熊夕人 <yuto.eguma@gmail.com>
2018-09-01 16:04:26 +0900

    undecorate document author test because using cache

--- src/test/java/org/dbflute/intro/app/logic/document/DocumentAuthorLogicTest.java
+++ src/test/java/org/dbflute/intro/app/logic/document/DocumentAuthorLogicTest.java
@@ -22,49 +22,14 @@ import org.dbflute.intro.unit.UnitIntroTestCase;
  */
 public class DocumentAuthorLogicTest extends UnitIntroTestCase {
 
-    // ===================================================================================
-    //                                                             Pre and Post Processing
-    //                                                             =======================
-    // -----------------------------------------------------
-    //                                             Attribute
-    //                                             ---------
-    private String originalUserName;
-
-    // -----------------------------------------------------
-    //                                            Processing
-    //                                            ----------
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-        this.originalUserName = System.getProperty(DocumentAuthorLogic.USER_NAME_KEY);
-    }
-
-    @Override
-    public void tearDown() throws Exception {
-        recoverSystemProperty(DocumentAuthorLogic.USER_NAME_KEY, originalUserName);
-        super.tearDown();
-    }
-
-    private void recoverSystemProperty(String key, String cachedProperty) {
-        if (cachedProperty != null) {
-            System.setProperty(key, cachedProperty);
-        } else {
-            System.clearProperty(key);
-        }
-    }
-
-    // ===================================================================================
-    //                                                                                Test
-    //                                                                                ====
     public void test_getAuthor() throws Exception {
         // ## Arrange ##
         final DocumentAuthorLogic logic = new DocumentAuthorLogic();
         inject(logic);
-        final String userName = "nanako.kono";
-        System.setProperty(DocumentAuthorLogic.USER_NAME_KEY, userName);
+        final String userName = System.getProperty(DocumentAuthorLogic.USER_NAME_KEY);
 
         // ## Act ##
-        String author = logic.getAuthor();
+        final String author = logic.getAuthor();
 
         // ## Assert ##
         assertEquals(userName, author);
