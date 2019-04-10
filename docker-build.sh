8bdee0f24943cd174a521e7cd1ce99f1223db282
江熊夕人 <yuto.eguma@gmail.com>
2019-04-10 22:21:18 +0900

    fix docker build script

---
+++ docker-build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+TAG=`git tag | tail -1f`
+VERSION=`echo ${TAG} | sed s/dbflute-intro-//g`
+
+echo "TAG      : ${TAG}"
+echo "VERSION  : ${VERSION}"
+
+## git checkout ${TAG}
+
+docker build . -t dbflute-intro --no-cache
+docker tag dbflute-intro dbflute/dbflute-intro:${VERSION}
+docker tag dbflute-intro dbflute/dbflute-intro:latest
+
+## docker push dbflute/dbflute-intro:${TAG}
