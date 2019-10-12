98c455cea30b0710145dadbdc5c92be485c2c692
江熊夕人 <yuto.eguma@gmail.com>
2019-10-12 23:32:37 +0900

    add license, consider decomment server mode

--- src/main/java/org/dbflute/intro/app/web/alter/AlterAction.java
+++ src/main/java/org/dbflute/intro/app/web/alter/AlterAction.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.dbflute.intro.app.web.alter;
 
 import java.util.List;
@@ -8,6 +23,7 @@ import javax.annotation.Resource;
 import org.dbflute.intro.app.logic.document.AlterSqlBean;
 import org.dbflute.intro.app.logic.playsql.migrate.PlaysqlMigrateLogic;
 import org.dbflute.intro.app.web.base.IntroBaseAction;
+import org.dbflute.intro.bizfw.annotation.NotAvailableDecommentServer;
 import org.dbflute.intro.mylasta.action.IntroMessages;
 import org.dbflute.util.DfStringUtil;
 import org.lastaflute.web.Execute;
@@ -84,6 +100,7 @@ public class AlterAction extends IntroBaseAction {
     //                                                                             Prepare
     //                                                                             =======
     @Execute(urlPattern = "{}/@word")
+    @NotAvailableDecommentServer
     public JsonResponse<Void> prepare(String clientProject) {
         playsqlMigrateLogic.unzipCheckedAlterZip(clientProject);
         playsqlMigrateLogic.copyUnreleasedAlterDir(clientProject);
@@ -94,6 +111,7 @@ public class AlterAction extends IntroBaseAction {
     //                                                                              Create
     //                                                                              ======
     @Execute(urlPattern = "{}/@word")
+    @NotAvailableDecommentServer
     public JsonResponse<Void> create(String clientProject, AlterCreateBody body) {
         validate(body, messages -> moreValidate(clientProject, body, messages));
         playsqlMigrateLogic.createAlterSql(clientProject, body.alterFileName);
