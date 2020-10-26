c78fbae645771dc95c0119f61f454f77102821f0
yuto.eguma <yuto.eguma@gmail.com>
2020-10-26 12:54:57 +0900

    #251 fix copyright

--- src/test/java/org/dbflute/intro/bizfw/code/LicenseManagementTest.java
+++ src/test/java/org/dbflute/intro/bizfw/code/LicenseManagementTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2019 the original author or authors.
+ * Copyright 2014-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,10 +29,11 @@ import org.lastaflute.di.exception.IORuntimeException;
 /**
  * @author hakiba
  * @author jflute
+ * @author cabos
  */
 public class LicenseManagementTest extends PlainTestCase {
 
-    private static final String COPYRIGHT = "Copyright 2014-2019 the original author or authors.";
+    private static final String COPYRIGHT = "Copyright 2014-2020 the original author or authors.";
 
     // ===================================================================================
     //                                                                      License Header
