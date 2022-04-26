3e09e2f0906feb8cc8304861d0323bc96e8d884e
hakiba <h031203yama@gmail.com>
2022-04-26 23:03:03 +0900

    #398 feat: welcome画面をi18n対応

--- frontend/src/static/app/plugin/i18n.js
+++ frontend/src/static/app/plugin/i18n.js
@@ -1,6 +1,8 @@
+import * as riot from 'riot'
 import i18next from 'i18next';
 import i18n_ja from '../../assets/i18n/locale-ja.json'
 import i18n_en from '../../assets/i18n/locale-en.json'
+import i18n from '../common/i18n.riot'
 
 i18next.init({
   lng: 'ja',
@@ -14,3 +16,5 @@ i18next.init({
     },
   }
 });
+
+riot.register('i18n', i18n)
