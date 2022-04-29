8c3c023d6e43dc8dafac755816367af97f894601
hakiba <h031203yama@gmail.com>
2022-04-29 21:18:54 +0900

    #398 refactor: index.jsとapp.riotへコメント & ルーティング処理のリファクタリング

--- frontend/src/static/app/index.js
+++ frontend/src/static/app/index.js
@@ -4,7 +4,9 @@ import 'semantic-ui-riot'
 import './plugin/i18n'
 import introPlugin from './plugin/IntroPlugin'
 
-const mountApp = riot.component(App)
+// 全てのComponentで共通的に利用する処理を登録する
 riot.install(introPlugin)
 
+// index.htmlのid=rootタグにapp.riotをマウントすることでriotアプリを起動する
+const mountApp = riot.component(App)
 mountApp(document.getElementById('root'))
