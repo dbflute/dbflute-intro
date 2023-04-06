8dbb68021d9ec89b48f1d45f5cd76a9453ade418
jflute <dbflute@gmail.com>
2023-04-06 20:21:41 +0900

    riot7, write comments for top files #460

--- frontend/src/@types/semantic-ui-riot.d.ts
+++ frontend/src/@types/semantic-ui-riot.d.ts
@@ -1,3 +1,10 @@
+// _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// semantic-ui-riotの関数をTypeScriptで補完されるようにするため。
+// 初期化時にriotにinstall()された関数を定義する。
+//
+// 最初から全部列挙しているわけではないので必要になったら随時追加しましょう。
+// TODO you 関数のコメントもあると嬉しいかも？ by jflute
+// _/_/_/_/_/_/_/_/_/_/
 export interface SemanticUiRiotPlugin {
   suLoading(b: boolean): void
   suConfirm(message: string): Promise<void>
--- frontend/src/static/app/index.ts
+++ frontend/src/static/app/index.ts
@@ -1,12 +1,35 @@
+/**
+ * ファーストアクセス時に実行されるトップのTypeScript。
+ * Riotを準備して、index.htmlのコンテンツの展開領域にアプリをマウントする。
+ *
+ * オフィシャルドキュメントのサンプルコードを参考に実装された。
+ * https://riot.js.org/ja/documentation/
+ */
+
+// riotモジュールのコンテンツすべてをimportする
+// (riot変数でexportされてるものにアクセスできる)
 import * as riot from 'riot'
+
+// アプリのriot関連の共通コンポーネントをimportする
+// (defaultでexportされてるものをAppという名前で扱って後にマウント)
 import App from './app.riot'
+
+// アプリ内でsemantic-ui-riotとsemantic-uiのグローバルオブジェクトを使うために
+// (初期化処理でui-riotの関数などがriotにinstallされる)
+// (初期化だけで良いので受け取りのための変数は特になし)
 import 'semantic-ui-riot'
+
+// 国際化対応の初期化処理を行うために
+// (初期化だけで良いので受け取りのための変数はない)
 import './shared/i18n'
+
+// アプリ独自のプラグイン関数をriotにinstall()するための関数
 import introPlugin from './app-plugin'
 
 // 全てのComponentで共通的に利用する処理を登録する
 riot.install(introPlugin)
 
 // index.htmlのid=rootタグにapp.riotをマウントすることでriotアプリを起動する
+// (component()関数でDOM操作のための関数を作成してHTML要素を指定)
 const mountApp = riot.component(App)
 mountApp(document.getElementById('root'))
