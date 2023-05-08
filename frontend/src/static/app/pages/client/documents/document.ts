5beeef9b9afacbc414093e3f33815ecf8b509b09
prprmurakami <prprmurakami@gmail.com>
2023-05-08 21:04:13 +0900

    #435 api.tsとdocument.tsの途中まで実装

---
+++ frontend/src/static/app/pages/client/documents/document.ts
@@ -0,0 +1,58 @@
+import i18n from '../../components/common/i18n.riot'
+import { api } from '../../../api/api'
+import { appRoutes } from '../../../app-router'
+// import { readFile } from '../../shared/io-utils'
+import { IntroRiotComponent, withIntroTypes } from '../../../app-component-types'
+import { DropdownItem } from '../../../components/dropdown/dropdown'
+
+interface State {}
+
+interface Document extends IntroRiotComponent<never, State> {
+  // ===================================================================================
+  //                                                                          Definition
+  //                                                                          =========
+
+  // ===================================================================================
+  //                                                                       Event Handler
+  //                                                                       =============
+
+  // ===================================================================================
+  //                                                                             Private
+  //                                                                             =======
+  prepareCurrentProject: () => void
+  prepareComponents: () => void
+  onRegisterModal: () => void
+  documentSettingModal: () => void
+  generateModal: () => void
+  showDocumentSettingModal: () => void
+  openSchemaHTML: () => void
+  openHistoryHTML: () => void
+  generateTask: () => void
+}
+export default withIntroTypes<Document>({
+  components: {
+    i18n,
+  },
+  state: {},
+
+  // ===================================================================================
+  //                                                                          Definition
+  //                                                                          ==========
+
+  // ===================================================================================
+  //                                                                           Lifecycle
+  //                                                                           =========
+  async onMounted() {
+    // this.prepareCurrentProject()
+    // this.prepareComponents()
+    // this.registerModalEvent()
+  },
+
+  // ===================================================================================
+  //                                                                       Event Handler
+  //                                                                       =============
+
+  // ===================================================================================
+  //                                                                             Private
+  //                                                                             =======
+})
