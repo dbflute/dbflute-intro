0867fa6b191cdbe81a663fb5c783281d70ffa548
jflute <dbflute@gmail.com>
2023-12-25 20:23:02 +0900

    refactor app-plugin.ts (generic, comment, exception) #486

--- frontend/src/static/app/app-plugin.ts
+++ frontend/src/static/app/app-plugin.ts
@@ -12,8 +12,8 @@ export interface DBFluteIntroPlugin {
   /**
    * Componentにclass名を動的に与えるためのオブジェクトを構築する
    * 具体例は [riotのマイグレーションガイド]{@link https://riot.js.org/ja/migration-guide/#%E3%83%86%E3%83%B3%E3%83%97%E3%83%AC%E3%83%BC%E3%83%88%E3%81%AE%E3%82%B7%E3%83%A7%E3%83%BC%E3%83%88%E3%82%AB%E3%83%83%E3%83%88}を参照
-   * @param classes - class list as object
-   * @returns return only the classes having a truthy value
+   * @param classes - class list as object (NotNull)
+   * @returns return only the classes having a truthy value (NotNull)
    */
   classNames: (classes: { [key: string]: boolean }) => string
 
@@ -30,21 +30,21 @@ export interface DBFluteIntroPlugin {
    * 指定されたselectorでHTMLのDOM要素を探す。(ないかもしれないときに使う)
    * - $関数で取得したElementに型を付与する際のヘルパー関数
    * - 複数のplugin関数で再利用するためglobalな関数として定義
-   * @param selector $関数に入れるselector文字列 e.g. [ref=jdbcDriverFqcn]
+   * @param selector $関数に入れるselector文字列 e.g. [ref=jdbcDriverFqcn] (NotNull)
    * @returns 対象のタグのHTMLオブジェクト (NullAllowed: if not found)
    */
-  elementAs: <HTMLElement extends Element>(selector: string) => HTMLElement | null
+  elementAs: <EL extends HTMLElement>(selector: string) => EL | null
 
   /**
    * 指定されたselectorでinputのDOM要素を探す。(ないかもしれないときに使う)
-   * @param selector $関数に入れるselector文字列 e.g. [ref=jdbcDriverFqcn]
+   * @param selector $関数に入れるselector文字列 e.g. [ref=jdbcDriverFqcn] (NotNull)
    * @returns 対象のタグのHTMLオブジェクト (NullAllowed: if not found)
    */
   inputElementBy: (selector: string) => HTMLInputElement | null
 
   /**
    * 指定されたselectorでinputのDOM要素を探す。(存在するはずのときに使う)
-   * @param selector $関数に入れるselector文字列 e.g. [ref=jdbcDriverFqcn]
+   * @param selector $関数に入れるselector文字列 e.g. [ref=jdbcDriverFqcn] (NotNull)
    * @returns 対象のタグのHTMLオブジェクト (NotNull: exception if not found)
    * @throws {ElementNotFoundError} 指定されたselectorに合致するinputのelementが存在しなかったら
    */
@@ -65,6 +65,14 @@ export interface DBFluteIntroPlugin {
 export class ElementNotFoundError extends Error {
   constructor(msg: string) {
     super(msg)
+
+    // #thinking ChatGPTに言われて、タイトル的なnameを入れてみた jflute (2023/12/25)
+    this.name = '指定されたselectorに対応する要素がないぞ例外'
+
+    // #thinking こうするとスタックトレースを保持される？的なことをChatGPTに言われて書いてみたが、いったん無しで試してみよう jflute (2023/12/25)
+    //if (Error.captureStackTrace) {
+    //  Error.captureStackTrace(this, ElementNotFoundError)
+    //}
   }
 }
 
@@ -98,8 +106,7 @@ const dbflutePlugin: DBFluteIntroPlugin = {
     })
   },
 
-  // #thinking jflute 実装インスタンスだけ T になってるのは、Javaで言う共変戻り値的な感じ？ (2023/09/25)
-  elementAs<T extends Element>(selector: string): T | null {
+  elementAs<EL extends HTMLElement>(selector: string): EL | null {
     return elementAs.bind(this, selector).apply() // 戻り型推論ちゃんと効いてるっぽい
   },
 
@@ -121,8 +128,9 @@ const dbflutePlugin: DBFluteIntroPlugin = {
 //                                          Element Handling
 //                                          ----------------
 // dbflutePluginの中で共通的に利用される関数をプライベート関数的に扱うために外に出している
-function elementAs<T extends Element>(selector: string): T {
-  return this.$(selector) as T
+function elementAs<EL extends HTMLElement>(selector: string): EL | null {
+  // #thinking jflute $()の戻りってHTMLElementじゃなくてElementだと思うんだけど、ノーチェックでいいのかな？ (2023/12/25)
+  return this.$(selector) as EL
 }
 
 // =======================================================================================
