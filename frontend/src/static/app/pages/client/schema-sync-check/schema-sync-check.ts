1e4d062a15494b826bffad72f9154a5a6d5a112d
yuto.eguma <yuto.eguma@gmail.com>
2024-01-07 15:46:45 +0900

    wip

--- frontend/src/static/app/pages/client/schema-sync-check/schema-sync-check.ts
+++ frontend/src/static/app/pages/client/schema-sync-check/schema-sync-check.ts
@@ -1,6 +1,12 @@
 import i18n from '../../components/common/i18n.riot'
 import { IntroRiotComponent, withIntroTypes } from '../../../app-component-types'
+import LatestResult from '../latest-result.riot'
 import { api } from '../../../api/api'
+
+type SchemaSyncCheckLatestResult = {
+  success: boolean
+  content: string
+}
 
 interface Props {
   projectName: string
@@ -8,28 +14,54 @@ interface Props {
 
 interface State {
   syncSchemaSetting: DfpropSchemasyncResult
+  latestResult?: SchemaSyncCheckLatestResult
 }
 
 interface SchemaSyncCheck extends IntroRiotComponent<Props, State> {
-  prepareSyncSchemaSetting: () => Promise<void>
+  prepareComponents: () => Promise<void>
+
+  // private
+  updateContents: () => Promise<void>
 }
 
 export default withIntroTypes<SchemaSyncCheck>({
-  //===================================================================================
+  components: {
+    LatestResult,
+  },
+  state: {
+    syncSchemaSetting: {},
+    latestResult: undefined,
+  },
+
+  // ===================================================================================
   //                                                                           Lifecycle
   //                                                                           =========
   /**
    * マウント完了時の処理。
    */
   async onMounted() {
-    console.log('mounted')
-    await this.prepareSyncSchemaSetting()
+    await this.prepareComponents()
   },
 
-  async prepareSyncSchemaSetting() {
+  async prepareComponents() {
+    await this.updateContents()
+  },
+
+  async updateContents() {
     const projectName = this.props.projectName
-    api.syncSchema(projectName).then((body) => {
+    const syncSchemaSetting = await api.syncSchema(projectName)
+    const latestResult = await api.latestResult(projectName, 'schemaSyncCheck').then((body) => {
       console.log(body)
+      if (body) {
+        return {
+          success: body.fileName.includes('success'),
+          content: body.content,
+        }
+      }
+    })
+    this.update({
+      syncSchemaSetting,
+      latestResult,
     })
   },
 })
