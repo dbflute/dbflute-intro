5d4f4ad2d8a8d37d3b011393ba4833d03add9960
jflute <jflute@localhost>
2026-03-20 22:42:43 +0900

    task-execute-modal.tsを途中まで整理整頓、主にコメント入れ #533

--- frontend/src/static/app/pages/client/task-execute-modal.ts
+++ frontend/src/static/app/pages/client/task-execute-modal.ts
@@ -1,13 +1,33 @@
 import { IntroRiotComponent, withIntroTypes } from '../../app-component-types'
 
+// #thinking jflute None はどういう状態を想定しているものなのか？モーダルを出さない便宜上のデフォルトの状態？ (2026/03/20)
+// task-execute-modalタグは常に評価されるから、最初の画面描画時などタスク実行してない時はNoneで何も起きないようにしている？
+/**
+ * DBFluteタスク実行ステータス。
+ */
 export type TaskExecuteStatus = 'None' | 'Executing' | 'Completed'
 
+// > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > >
+// ^                                                                                     v
+// ^                                                                                     v
+// ^                                                                                     v
+// < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < <
+
+/**
+ * タスク実行結果のメッセージなどタグ引数で受け取る。
+ */
 interface Props {
+  /** モーダルに表示するIntroユーザー向けメッセージ。 */
   message: string
+
+  /** DBFluteタスク実行ステータス。 */
   status: TaskExecuteStatus
+
+  /** モーダルを閉じた時の処理のフック。呼び出し側が好きな処理を実行できるように。 (EmptyAllowed: 好きな処理なければ) */
   onModalHide?: () => void
 }
 
+// #thinking jflute onBeforeUpdate()にてstatusをPropsから受け取ってchangeさせてるけど、Propsのstatusを全部直接使うじゃダメなのかな？ (2026/03/20)
 interface State {
   status: TaskExecuteStatus
 }
@@ -37,12 +57,24 @@ const EXECUTING_MODAL: SuModal = {
   buttons: [],
 }
 
+// > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > >
+// ^                                                                                     v
+// ^                                                                                     v
+// ^                                                                                     v
+// < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < <
+
 interface TaskExecuteModal extends IntroRiotComponent<Props, State> {
   onBeforeUpdate(): void
   show(): boolean
   modal(): SuModal | undefined
   onHide(): void
 }
+
+// > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > >
+// ^                                                                                     v
+// ^                                                                                     v
+// ^                                                                                     v
+// < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < <
 
 export default withIntroTypes<TaskExecuteModal>({
   state: {
