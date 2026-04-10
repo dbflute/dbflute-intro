2f4a38dd35742b364e263c1359666ddabde833e7
jflute <jflute@localhost>
2026-04-10 22:42:34 +0900

    latest-resultのコメント入れ少しやってみた #533

--- frontend/src/static/app/pages/client/latest-result.ts
+++ frontend/src/static/app/pages/client/latest-result.ts
@@ -1,12 +1,28 @@
 import { IntroRiotComponent, withIntroTypes } from '../../app-component-types'
 import Raw from '../../components/common/raw.riot'
 
+// > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > >
+// ^                                                                                     v
+// ^                                                                                     v
+// ^                                                                                     v
+// < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < <
+
 interface Props {
+  /** 現在対象としているDBFluteクライアントをプロジェクト名 e.g. maihamadb */
   projectName: string
-  task: string
+
+  /** その直近のDBFluteタスクの名前 */
+  task: string // #thinking jflute unusedに見えるが、いつか使うかもで念のため受けってる？ (2026/04/10)
+
+  /** その直近のDBFluteタスク実行が "成功" で終了したか？ */
   success: boolean
+
+  /** 表示領域のヘッダー (タイトル表示) を表示するかどうか？ */
   showHeader: boolean
-  headerTitle?: string
+
+  /** 表示領域のヘッダーに表示するタイトル文字列 */
+  headerTitle?: string // #thinking jflute ないかも項目だけど、.riotでそのまま使ってるけどOK？ (2026/04/10)
+
   resultTitle: string
   resultMessage?: string
   content?: string
@@ -19,10 +35,22 @@ interface State {
   showContent: boolean
 }
 
+// > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > >
+// ^                                                                                     v
+// ^                                                                                     v
+// ^                                                                                     v
+// < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < <
+
 interface LatestResult extends IntroRiotComponent<Props, State> {
   onMounted(): void
   toggleLatestResult(): void
 }
+
+// > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > >
+// ^                                                                                     v
+// ^                                                                                     v
+// ^                                                                                     v
+// < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < <
 
 export default withIntroTypes<LatestResult>({
   components: {
