78cc4d50c526ad89c204b769d401952d576c66b4
jflute <jflute@localhost>
2026-08-04 22:44:48 +0900

    SchemaHTMLへのリンク制御のところに、コメントをちょっと入れた #352

--- src/main/java/org/dbflute/intro/app/logic/document/DocumentDisplayLogic.java
+++ src/main/java/org/dbflute/intro/app/logic/document/DocumentDisplayLogic.java
@@ -79,8 +79,12 @@ public class DocumentDisplayLogic {
                 if (line.contains("<a href=\"./history-" + projectName + ".html\">to HistoryHTML</a>")) {
                     line = "<a href=\"/api/document/" + projectName + "/historyhtml\">to HistoryHTML</a>";
                 }
+
+                // 主に、SchemaHTML以外 (e.g. HistoryHTML) のSchemaHTMLへのリンク。
+                // HistoryHTMLであるようなテーブルリンクも一緒に制御できるようにしている。 (2026/08/04)
                 line = line.replace("href=\"./schema-" + projectName + ".html", // preserve fragment e.g. #member
                         "href=\"/api/document/" + projectName + "/schemahtml");
+
                 if (line.contains("<a href=\"./properties-" + projectName + ".html\">to PropertiesHTML</a>")) {
                     line = "<a href=\"/api/document/" + projectName + "/propertieshtml\">to PropertiesHTML</a>";
                 }
